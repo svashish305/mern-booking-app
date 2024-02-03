46da7950e2219b622cb1c3ff010e8d83c0692e61
Shubhashish Verma <svashish305@gmail.com>
2024-02-03 14:30:52 +0530

    added edit hotel feature and added e2e test

--- backend/src/routes/my-hotels.ts
+++ backend/src/routes/my-hotels.ts
@@ -57,6 +57,57 @@ router.get("/", verifyToken, async (req: Request, res: Response) => {
 	}
 });
 
+router.get("/:id", verifyToken, async (req: Request, res: Response) => {
+	const id = req.params.id.toString();
+	try {
+		const hotel = await Hotel.findOne({
+		_id: id,
+		userId: req.userId,
+		});
+		res.json(hotel);
+	} catch (error) {
+		res.status(500).json({ message: "Error fetching hotels" });
+	}
+});
+  
+router.put(
+	"/:hotelId",
+	verifyToken,
+	upload.array("imageFiles"),
+	async (req: Request, res: Response) => {
+		try {
+			const updatedHotel: HotelType = req.body;
+			updatedHotel.lastUpdated = new Date();
+
+			const hotel = await Hotel.findOneAndUpdate(
+				{
+				_id: req.params.hotelId,
+				userId: req.userId,
+				},
+				updatedHotel,
+				{ new: true }
+			);
+
+			if (!hotel) {
+				return res.status(404).json({ message: "Hotel not found" });
+			}
+
+			const files = req.files as Express.Multer.File[];
+			const updatedImageUrls = await uploadImages(files);
+
+			hotel.imageUrls = [
+				...updatedImageUrls,
+				...(updatedHotel.imageUrls || []),
+			];
+
+			await hotel.save();
+			res.status(201).json(hotel);
+		} catch (error) {
+			res.status(500).json({ message: "Something went wrong" });
+		}
+	}
+);
+
 async function uploadImages(imageFiles: Express.Multer.File[]) {
 	const uploadPromises = imageFiles.map(async (image) => {
 		const b64 = Buffer.from(image.buffer).toString("base64");
@@ -67,7 +118,7 @@ async function uploadImages(imageFiles: Express.Multer.File[]) {
   
 	const imageUrls = await Promise.all(uploadPromises);
 	return imageUrls;
-}
+};
 
 export default router;
 
